b6230711a18bdfb9dc39c328efe620c9a6571af7
unknown <Raghav12698@gmail.com>
2024-08-08 15:15:17 +0530

    Day14

--- Week4/Day3/Cheezy.java
+++ Week4/Day3/Cheezy.java
@@ -28,13 +28,15 @@ public class Cheezy
         {
             return false;
         }
+        //include current value in answer/
         sum+=arr[i];
+        //check if this can be included in final answer
         if(!backtracksumArray(arr, i+1, k))
         {
             sum-=arr[i];
         }
         return true;
-    }
+    } 
     public static void main(String[] args) 
     {
         int [] arr={1,2,3,4,5,6,7};
